0ecc43dc232638563d3f32ff591146afb1ddc93e
Nils Kolvenbach <nils.kolvenbach@7p-group.com>
2020-01-28 18:30:30 +0100

    Added meta description for home page and enabled evergreen setting for smaller builds.

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -11,6 +11,7 @@ module.exports = {
     ['meta', { name: 'msapplication-TileColor', content: '#eef3f2' }],
     ['meta', { name: 'theme-color', content: '#eef3f2' }]
   ],
+  evergreen: true,
   locales: {
     // The key is the path for the locale to be nested under.
     // As a special case, the default locale can use '/' as its path.
